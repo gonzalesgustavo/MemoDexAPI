4f6d9b08e40f0522a31483e50293a97ffc79cdeb
gonzalesgustavo <gcodingconnected@outlook.com>
2019-12-17 11:36:55 -0700

    Altered Response class, Implimented responses to Contact Routes.

--- Utils/ResponseBuilder.js
+++ Utils/ResponseBuilder.js
@@ -1,3 +1,6 @@
+
+const generalErrorMessage = "Possible server error or id/data failed to meet API criteria please refer to the documentation for route specific requirements";
+
 module.exports = class Response {
     static success(message, status, data, success) {
         return {
@@ -7,19 +10,19 @@ module.exports = class Response {
             result: data
         }
     }
-    static failed(message, status, error, success) {
+    static failed(error, statusCode = 404, message = generalErrorMessage) {
         return {
-            success,
+            success: false,
             message,
-            status,
+            status: statusCode,
             result: error
         }
     }
-    static noContent(message, status, success) {
+    static noContent(caller) {
         return {
-            success,
-            message,
-            status,
+            success: true,
+            message: `${caller} was removed successfully`,
+            status: 200,
             result: null
         }
     }
